41318d222700b826c24ab7752fa18d455ce2a229
Ruben Jenster <r.jenster@drachenfels.de>
2020-09-09 16:03:00 +0200

    Add test for relative mount path resolution.
    Signed-off-by: Ruben Jenster <r.jenster@drachenfels.de>

--- cmd/utils_test.go
+++ cmd/utils_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/apex/log"
 	"github.com/stretchr/testify/require"
 	"io/ioutil"
 	"os"
@@ -11,7 +12,11 @@ import (
 	"testing"
 )
 
-func TestResolveMountDestination(t *testing.T) {
+func init() {
+	log.SetLevel(log.DebugLevel)
+}
+
+func TestResolveMountDestinationAbsolute(t *testing.T) {
 	tmpdir, err := ioutil.TempDir("", "golang.test")
 	require.NoError(t, err)
 	defer os.RemoveAll(tmpdir)
@@ -27,6 +32,39 @@ func TestResolveMountDestination(t *testing.T) {
 	require.NoError(t, err)
 	err = ioutil.WriteFile(filepath.Join(tmpdir, "folder3", "test.txt"), []byte("hello"), 0640)
 	require.NoError(t, err)
+
+	p, err := resolveMountDestination(tmpdir, "/folder1/f2/f3/test.txt")
+	require.Equal(t, filepath.Join(tmpdir, "/folder3/test.txt"), p)
+	require.NoError(t, err)
+
+	p, err = resolveMountDestination(tmpdir, "/folder1/f2/xxxxx/fooo")
+	require.Equal(t, filepath.Join(tmpdir, "/folder2/xxxxx/fooo"), p)
+	require.Error(t, err, os.ErrExist)
+
+	p, err = resolveMountDestination(tmpdir, "/folder1/f2/f3/hello.txt")
+	require.Equal(t, filepath.Join(tmpdir, "/folder3/hello.txt"), p)
+	require.Error(t, err, os.ErrExist)
+}
+
+func TestResolveMountDestinationRelative(t *testing.T) {
+	tmpdir, err := ioutil.TempDir("", "golang.test")
+	require.NoError(t, err)
+	defer os.RemoveAll(tmpdir)
+	err = os.MkdirAll(filepath.Join(tmpdir, "folder1"), 0750)
+	require.NoError(t, err)
+	err = os.MkdirAll(filepath.Join(tmpdir, "folder2"), 0750)
+	require.NoError(t, err)
+	err = os.MkdirAll(filepath.Join(tmpdir, "folder3"), 0750)
+	require.NoError(t, err)
+	err = os.Symlink("../folder2", filepath.Join(tmpdir, "folder1", "f2"))
+	require.NoError(t, err)
+	err = os.Symlink("../folder3", filepath.Join(tmpdir, "folder2", "f3"))
+	require.NoError(t, err)
+	err = ioutil.WriteFile(filepath.Join(tmpdir, "folder3", "test.txt"), []byte("hello"), 0640)
+	require.NoError(t, err)
+
+	//err = os.Symlink("../../folder2", filepath.Join(tmpdir, "folder1", "f2"))
+	//require.NoError(t, err)
 
 	p, err := resolveMountDestination(tmpdir, "/folder1/f2/f3/test.txt")
 	require.Equal(t, filepath.Join(tmpdir, "/folder3/test.txt"), p)
@@ -74,7 +112,7 @@ func TestIsStaticBinary(t *testing.T) {
 }
 
 func TestCapabilities(t *testing.T) {
-	require.NoError(t, runtimeHasCapabilitySupport("/usr/local/bin/startcontainer"))
+	require.NoError(t, runtimeHasCapabilitySupport("/usr/local/bin/crio-lxc-start"))
 	require.Error(t, runtimeHasCapabilitySupport("/bin/zcat"))
 }
 
